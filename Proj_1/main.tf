670e00fc5a1160da6e4e14510a23898b3e231b0c
Macko <prvinsm21@gmail.com>
2023-10-11 03:06:50 +0530

    Files added

--- Proj_1/main.tf
+++ Proj_1/main.tf
@@ -10,3 +10,11 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "demo" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  tags = {
+    Name = "server1"
+  }
+}
---
+++ Proj_1/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_value = "ami-xxxxxxxxxx"
+instance_type_value = "t2.micro"
