3349b8aa9112f75c4c753dcbe7467ea3825e686e
Macko <prvinsm21@gmail.com>
2023-10-12 18:27:26 +0530

    Files added

--- Proj_2/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "frontend" {
-  ami = "ami-0261755bbcb8c4a84"
-  instance_type = "t2.micro"
-  tags = {
-    Name = var.instance_name
-  }
-}
-
--- Proj_2/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "instance_name" {
-  description = "Value of Name tag for EC2 instance"
-  type = string
-  default = "Appliance-Server"
-}
